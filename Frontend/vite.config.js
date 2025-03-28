b48ec5f0948916bdd8fc69a096c1f66635b7226c
Saimiksha <saimiksha.agarwal@cumminscollege.in>
2025-03-29 00:36:25 +0530

    Fix GitHub Pages deployment

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/FishCompass/', // Set this to match your repository name
   plugins: [react(),
   tailwindcss()
   ],
